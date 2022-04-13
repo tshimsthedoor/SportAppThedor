fca8f8a152181e7ebb321768684633283a221d1f
Tresor Tshimanga <tshims79@gmail.com>
2022-04-13 16:32:28 +0200

    Add Model
    - Added The product model

---
+++ SportAppThedor/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportAppThedor.Models
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ Test/SportAppThedor.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SportAppThedor.Tests
+{
+    public class Class1
+    {
+    }
+}
