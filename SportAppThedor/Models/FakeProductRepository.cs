d462d586b6e857c5c3c942df488d3b0dafe2bbb9
Tresor Tshimanga <tshims79@gmail.com>
2022-04-13 16:38:24 +0200

    Create a fake repostory
    - I create a fake repository that inherit from the interface and I implement the IEnumerable interface

---
+++ SportAppThedor/Models/FakeProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportAppThedor.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>;
+    }
+}
